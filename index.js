752f19a93815210cbedeec27554840387027c666
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-10-19 22:44:58 +0530

    add basic get request

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import express from 'express'
 
 const app = express() 
 
+app.get("/",
+    (req,res)=>{
+        console.log("Hello world")
+    }
+)
+
 
 app.listen(5000,(req,res)=>{
     console.log("Server is Running on port 5000")
 });
+
+
