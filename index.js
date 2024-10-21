3b6459b0864c8a29b24c75878c79c4bb21be24f1
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-10-21 14:38:18 +0530

    mongose install for build the database connectivity

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express()
 
 app.use(bodyParser.json())  //middleware
 
+
+const connectiionString = "mongodb+srv://tester2:123@cluster0.wd7xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 app.use("/api/users",userRouter)
+
+
 
 app.listen(5000,(req,res)=>{
     console.log("Server is Running on port 5000")
