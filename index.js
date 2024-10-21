e2485faf15ae37823ff31b3d904bfddbb8f15c58
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-10-21 09:29:57 +0530

    now clean the index .js file and import we are early created uerRoute file and use it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+import userRouter from './routes/userRoute.js'
 
 
 
@@ -7,32 +8,7 @@ const app = express()
 
 app.use(bodyParser.json())  //middleware
 
-app.get("/",
-    (req,res)=>{
-        console.log("get request")
-
-
-        res.json({
-            message : "this is response message"
-        })
-
-    }
-)
-
-app.post("/",(req,res)=>{
-    console.log("This is a post request");
-
-
-    const name = req.body.name;
-
-    const message = "this is new : "+name
-    console.log(message)
-
-    res.status(200).json({
-        message : message
-    })
-})
-
+app.use("/users",userRouter)
 
 app.listen(5000,(req,res)=>{
     console.log("Server is Running on port 5000")
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,27 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.get("/users",(req,res)=>{
+
+//user get part
+userRouter.get("/",(req,res)=>{
 
     res.json({
         message : "user get request"
     })
 })
+
+
+//user post part
+userRouter.post("/",(req,res)=>{
+
+    const name = req.body.name;
+
+    res.json({
+        message : "user post request",
+        name : name
+    })
+})
+
+export default userRouter;
