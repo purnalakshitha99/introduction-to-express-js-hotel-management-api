b4bcaac449be88345f847c2cdb75591a9eb62abd
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-10-26 11:56:49 +0530

    now login user password and db password compair using bcript and match the enterd password and stored password

--- controller/userController.js
+++ controller/userController.js
@@ -3,23 +3,24 @@ import User from '../model/user.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
-export function getUser(req,res){
+export function getUser(req, res) {
     console.log("get user")
     User.find().then(
-    (userList)=>{
+        (userList) => {
 
-        res.json({
-            list : userList
-        })
-    }
-).catch(
-    (error)=>{
-        res.json({
-            message : "get failed",
-            details : error.message
-        })
-    }
-)}
+            res.json({
+                list: userList
+            })
+        }
+    ).catch(
+        (error) => {
+            res.json({
+                message: "get failed",
+                details: error.message
+            })
+        }
+    )
+}
 
 
 export function postUser(req, res) {
@@ -65,66 +66,87 @@ export function postUser(req, res) {
 }
 
 
-export function loginUser(req,res){
+export function loginUser(req, res) {
 
     const credential = req.body
 
-    User.findOne({email : credential.email, password : credential.password}).then(
-        (user)=>{
-            if(user == null){
-                
+    console.log(credential)
+
+    const enteredPassword = credential.password;
+
+
+
+    User.findOne({ email: credential.email }).then(
+        (user) => {
+            if (user == null) {
+
                 res.status(404).json(
                     {
-                        message : "User not found"
+                        message: "User not found"
                     }
                 )
-            }else{
+            } else {
 
-                const payload = {
-                    id : user._id,
-                    email : user.email,
-                    firstName : user.firstName,
-                    lastName : user.lastName,
-                    type : user.type
+                const isPasswordValid = bcrypt.compare(credential.password, user.password);
+
+                if (!isPasswordValid) {
+
+                    return res.status(403).json({
+                        message: "Incorrect Password"
+                    })
+                } else {
+                    const payload = {
+                        id: user._id,
+                        email: user.email,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        type: user.type
+                    }
+
+                    const token = jwt.sign(payload, "secret", { expiresIn: "1h" });
+
+                    return res.json({
+                        message: "user found",
+                        user: user,
+                        token: token
+                    })
+
                 }
 
-                const token = jwt.sign(payload,"secret",{expiresIn:"1h"});
-                
-                res.json({
-                    message : "user found",
-                    user : user,
-                    token : token
-                })
             }
+
+
         }
     )
 }
+    
 
-export function updateUser(req,res){
+
+export function updateUser(req, res) {
 
     res.json({
-        message : "user update request"
+        message: "user update request"
     })
 
 }
 
-export function deleteUser(req,res){
-    
+export function deleteUser(req, res) {
+
     const email = req.body.email;
 
-    User.deleteOne({email : email}).then(
-        ()=>{
+    User.deleteOne({ email: email }).then(
+        () => {
             res.json(
                 {
-                    message : "user delete successfully"
+                    message: "user delete successfully"
 
                 }
             )
         }
     ).catch(
-        ()=>{
+        () => {
             res.json({
-                message : "user delete failed"
+                message: "user delete failed"
             })
         }
     )
