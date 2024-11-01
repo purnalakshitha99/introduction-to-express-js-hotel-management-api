09b2d5c891606a9589864262b739827d675a3256
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-02 00:01:05 +0530

    add room creation fucntion in controller

---
+++ controller/roomController.js
@@ -0,0 +1,43 @@
+import Room from "../model/room";
+
+export function createRoom(req,res){
+
+    if (user == null) {
+        return res.status(401).json({
+            message: "Please login to create a room"
+        });
+    }
+
+    if (user.type != "admin") {
+        return res.status(403).json(
+            {
+                message: "cant access for create room"
+            }
+        )
+    }
+
+    const room = req.body;
+    console.log(room)
+
+    const newRoom = new Room(room);
+
+    newRoom.save().then(
+        (room)=> {
+            console.log("room creation success")
+            return res.json(
+                {
+                    message: "room creation Succuess",
+                    room : room
+                }
+            );
+        }
+    ).catch(
+        (err) => {
+            console.log("room creation failed")
+            return res.status(500).json({
+                message: "room creation failed",
+                err: err.message
+            });
+        }
+    )
+}
