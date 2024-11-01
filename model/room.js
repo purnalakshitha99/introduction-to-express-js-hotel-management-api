3bb45f2649511dab305e5f2aae3a791527f1f365
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-01 23:54:28 +0530

    creating all attributes in room model

--- model/room.js
+++ model/room.js
@@ -6,7 +6,44 @@ const roomSchema = mongoose.Schema(
         type : Number,
         required : true,
         unique : true
+       },
+
+       category : {
+        type : String,
+        required : true
+       },
+
+       maxGuests : {
+        type : Number,
+        required : true
+       },
+
+       available : {
+        type : Boolean,
+        required : true,
+        default : true
+       },
+
+       photos : [
+        {
+            type : String
+        }
+       ],
+
+       specialDescription : {
+        type : String,
+        default : ""
+       },
+
+       notes : {
+        type : String,
+        default : ""
        }
         
     }
 )
+
+
+const Room = roomSchema.model("rooms",roomSchema);
+
+export default Room;
