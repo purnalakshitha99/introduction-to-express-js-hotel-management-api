4651677ad85de742622dfe1288915ff51fa10b4c
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-01 23:48:37 +0530

    adding room json to primary attribute unique

--- model/room.js
+++ model/room.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const roomSchema = mongoose.Schema(
     {
+       roomId : {
+        type : Number,
+        required : true,
+        unique : true
+       }
         
     }
 )
