a9cf43e0eebd225a030b394ac6f9e47e8bdfe025
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-02 20:11:42 +0530

    add room route to get room route

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { createRoom } from '../controller/roomController.js';
+import { createRoom,getRooms } from '../controller/roomController.js';
 
 const roomRoute = express.Router();
 
 roomRoute.post("/",createRoom);
+roomRoute.get("/",getRooms);
+
 
 
 export default roomRoute;
