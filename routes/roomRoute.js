7d2dc6d133f36a53a09a4edc0599d5ef1dec3c9a
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-16 10:58:24 +0530

    adding new route for room delete by room id params

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createRoom,deleteRoom,getRooms } from '../controller/roomController.js';
+import { createRoom,deleteRoom,deleteRoomByParam,getRooms } from '../controller/roomController.js';
 
 const roomRoute = express.Router();
 
 roomRoute.post("/",createRoom);
 roomRoute.get("/",getRooms);
 roomRoute.delete("/",deleteRoom);
+roomRoute.delete("/:id",deleteRoomByParam);
 
 
 
