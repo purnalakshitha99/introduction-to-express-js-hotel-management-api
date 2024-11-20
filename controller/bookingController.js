5113e8fdacb82bbb0d5d0b042502f9cc4988d4b1
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-11-20 08:49:00 +0530

    add bookin  create method to get document records count check

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -1,2 +1,12 @@
 import Booking from "../model/booking.js";
 
+export function createBooking(req,res){
+
+    var startingId = 1000;
+
+    Booking.countDocuments({}).then(
+        (count)=>{
+            console.log(count);
+        }
+    )
+}
