6886048f1fa16a2a0a2ff43bfb8878014eeb01ca
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-12-15 23:58:05 +0530

    check the you are entered roomId is room table have or doesnt have

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -1,47 +1,111 @@
 import Booking from "../model/booking.js";
+import Room from "../model/room.js";
 import { isAdmin, isCustomer } from "./userController.js";
 
-export function createBooking(req, res) {
+// export function createBooking(req, res) {
 
-    const validCustomer = isCustomer(req, res)
+//     const validCustomer = isCustomer(req, res)
+
+//     if (!validCustomer) {
+//         return
+//     }
+
+//     const startingId = 1200;
+// //methana count docment kiyana eken booking schema eke thiyena row count eka enawa
+//     Booking.countDocuments({}).then(
+//         (count) => {
+//             console.log(count);
+//             const newId = "INV" + startingId + count;
+
+//             Room.findOne({roomId : req.body.roomId}).then(
+//                 (room)=>{ 
+//                     if(!result){
+//                         return res.status(404).json({
+//                             message : "room not found"
+//                         })
+//                     }
+
+//                     console.log("room id is :::::::: "+room.roomId)
+//                     const newBooking = new Booking({
+//                         bookingId: newId,
+//                         roomId: room.roomId,
+//                         email: req.user.email,
+//                         startDate: req.body.startDate,
+//                         endDate: req.body.endDate
+//                     })
+        
+//                     newBooking.save().then(
+//                         (result) => {
+//                             return res.json({
+//                                 message: "successfully saved",
+//                                 result: result
+//                             })
+//                         }
+//                     ).catch(
+//                         (err) => {
+//                             res.status(500).json({
+//                                 message: "booking failed",
+//                                 details: err.message
+//                             })
+//                         }
+//                     )
+
+//                 }
+//             )
+//             })
+
+            
+
+//         }
+//     )
+// }
+
+
+export function createBooking(req, res) {
+    const validCustomer = isCustomer(req, res);
 
     if (!validCustomer) {
-        return
+        return;
     }
 
     const startingId = 1200;
-//methana count docment kiyana eken booking schema eke thiyena row count eka enawa
-    Booking.countDocuments({}).then(
-        (count) => {
-            console.log(count);
-            const newId = "INV" + startingId + count;
+    // Methana count document kiyana eken booking schema eke thiyena row count eka enawa
+    Booking.countDocuments({}).then((count) => {
+        console.log(count);
+        const newId = "INV" + startingId + count;
 
+        Room.findOne({ roomId: req.body.roomId }).then((room) => {
+            if (!room) { // Fixed `result` to `room`
+                return res.status(404).json({
+                    message: "room not found",
+                });
+            }
+
+            console.log("room id is :::::::: " + room.roomId);
             const newBooking = new Booking({
                 bookingId: newId,
-                roomId: req.body.roomId,
+                roomId: room.roomId,
                 email: req.user.email,
                 startDate: req.body.startDate,
-                endDate: req.body.endDate
-            })
+                endDate: req.body.endDate,
+            });
 
-            newBooking.save().then(
-                (result) => {
+            newBooking
+                .save()
+                .then((result) => {
                     return res.json({
                         message: "successfully saved",
-                        result: result
-                    })
-                }
-            ).catch(
-                (err) => {
+                        result: result,
+                    });
+                })
+                .catch((err) => {
                     res.status(500).json({
                         message: "booking failed",
-                        details: err.message
-                    })
-                }
-            )
-
-        }
-    )
+                        details: err.message,
+                    });
+                });
+        });
+    });
 }
 
 export function getBookings(req, res) {
